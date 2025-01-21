9c4c907070886d907158b2a27477dda9f1d3b09c
BoolParty <nflpickparty7@gmail.com>
2025-01-20 19:02:00 -0800

    revert CORS to dev setup

--- server.js
+++ server.js
@@ -5,22 +5,13 @@ const cron = require('node-cron');
 const pickRoutes = require('./routes/picks');
 const Pick = require('./models/Pick');
 const cors = require('cors');
-const allowedOrigins = ['https://pickparty.net', 'http://localhost:3000'];
 const { sendMatchEmail } = require('./email');
 
 const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true); // Allow the request
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
+app.use(cors());
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)
