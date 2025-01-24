ddf7f15cab5329dac5b5fb0012faf1069980ff7e
BoolParty <nflpoolparty7@gmail.com>
2025-01-24 10:23:00 -0800

    removed 'type' from pickSchema, reverting to 1/23 night status

--- models/Pick.js
+++ models/Pick.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const pickSchema = new mongoose.Schema({
   user: { type: String, required: true, index: true }, // Index on user
   team: { type: String, required: true },
-  type: { type: String, enum: ['spread', 'total', 'moneyline'], required: true },
   spread: { type: String, required: true },
   wager: { type: Number, required: true },
   matched: { type: Boolean, default: false, index: true },
